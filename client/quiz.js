b9242b8d82d95f0540851b56c967045e3dd318df
pointbar <stephane.langlois@scopyleft.fr>
2016-01-26 12:19:59 +0100

    Check user answer

--- client/quiz.js
+++ client/quiz.js
@@ -1,3 +1,14 @@
 Template.answers.helpers({
-  answers: function () { return Quiz.findOne().answers }
+  answers: () => Quiz.findOne().answers
+})
+
+Template.answers.events({
+  'click input': (evt) => {
+    const quiz = Quiz.findOne()
+    const userAnswer = evt.target.value
+    if (quiz.answers[quiz.answer] === userAnswer)
+      Notifications.success('Bonne réponse', `c'est bien ${userAnswer}`)
+    else
+      Notifications.warn('Mauvaise réponse', 'You loose!')
+  }
 })
